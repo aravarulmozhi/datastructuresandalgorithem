feca7b7067678bd2995874c0433ce99f51c282fe
AravindR1 <>
2021-05-17 14:20:42 +0000

    revese string

---
+++ reversestring.js
@@ -0,0 +1,22 @@
+/*
+function reverseString(str){
+  //O(1)
+let strArray= str.split("") //O(n)
+let joinStr=[] //O(1)
+for(let i=strArray.length; i>=0; i--){ //O(n)
+  joinStr.push(strArray[i]) //O(1)
+}
+return joinStr.join(" ") //O(1)
+}
+console.log(reverseString("hello world"))
+*/
+
+/*const revString=(str)=>{
+let strArr=str.split("")
+return strArr.reverse().join("")
+}
+ console.log(revString("my name is aravind"))
+*/
+
+
+
